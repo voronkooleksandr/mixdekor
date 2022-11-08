f2108ff79e120a764aacb88ec671be78992885d4
Oleksandr <voronko.oleksandr@gmail.com>
2022-11-08 22:12:55 +0200

    create filter structure and its methods

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
   templateUrl: './products-header.component.html',
-  styleUrls: ['./products-header.component.scss']
+  styleUrls: ['./products-header.component.scss'],
 })
 export class ProductsHeaderComponent implements OnInit {
+  @Output() columnsCountChange = new EventEmitter<number>();
+  sort = 'спаданням';
+  itemShowItem = 12;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
   }
 
+  onItemUpdated(count: number): void { 
+    this.itemShowItem = count;
+  }
+
+  onColumnsUpdated(colsNumber: number): void {
+    this.columnsCountChange.emit(colsNumber);
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,19 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  cols = 3;
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onColumnsCountChange(colsNumber: number): void { 
+     this.cols = colsNumber;
   }
-
 }
