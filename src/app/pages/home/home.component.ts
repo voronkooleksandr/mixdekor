08821ce970eb5436c3443ed5a8818c3b42ff20f0
Oleksandr <voronko.oleksandr@gmail.com>
2022-11-13 22:08:08 +0200

    create product-box component and set up its style

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,10 @@
 import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: { [id: number]: number } = {
+  1: 400, 3: 335, 4: 350
+};
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,6 +12,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
 
   constructor() {}
@@ -16,9 +21,10 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChange(colsNumber: number): void {
     this.cols = colsNumber;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   }
 
   onShowCategory(newCategory: string): void {
-    this.category = newCategory;;
+    this.category = newCategory;
   }
 }
